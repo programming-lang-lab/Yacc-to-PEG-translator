6647ebd240e80081fd8cdb1042ecc57ce48b7400
hiro-ishii <h.ishii83@gmail.com>
2019-09-06 11:34:50 +0900

    git の conflict の解消

---
+++ generator/rats_generator.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+require './lib/generator.rb'
+
+class RatsGenerator < Generator
+  def generate filename
+    super
+    code = "module #{filename.split("/").last.slice(/\w+/)};\n\noption main(start);\n\npublic "
+
+    File.open(filename, "w"){|file|
+      @peg.each{|rule|
+        @indent.push rule.lh.size+5
+        code += "void #{rule.lh} = " 
+        
+        (rule.rh.size-1).times{|n|
+          code += generate_rh(rule.rh[n]) + "\n" + " "*@indent.last + " / "
+        }
+        
+        if rule.rh.size > 1
+          code += generate_rh(rule.rh.last) + "\n" +" "*(@indent.last+1) + ";\n\n"
+        else
+          code += generate_rh(rule.rh.last).rstrip + ";\n\n"
+        end
+        @indent.pop
+      }
+      file.write code
+    }
+
+    exit 0
+  end
+end
