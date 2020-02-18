4acac1e76560295c71bd0a10672508f4b8dd2170
hiro-ishii <h.ishii83@gmail.com>
2020-02-18 19:07:37 +0900

    h否定先読みを使用した際に出力する PEG 記述のインデントの修正

--- lib/generator.rb
+++ lib/generator.rb
@@ -6,6 +6,7 @@ class Generator
     @indents = [0]
     @choice_indent = 0
     @idt = 4
+    @neg_idt = 0
   end
 
   def generate filename
@@ -19,66 +20,44 @@ class Generator
     send("join_#{rh.class.name.downcase}", rh)
   end
 
-  def join_string str, out_of_array = true
+  def join_string str
     str + " "
   end
 
-  def join_array ary, out_of_array = true
+  def join_array ary
     code = ""
     ary.each do |sym|
-      code += send("join_#{sym.class.name.downcase}", sym, out_of_array)
+      code += send("join_#{sym.class.name.downcase}", sym)
     end
     code
   end
 
-  def join_repeat rpt, out_of_array = true
+  def join_repeat rpt
     code = send("join_#{rpt.child.class.name.downcase}", rpt.child)
-    ary = rpt.child
-    if judge_param code, ary
-      code = "(" + code.rstrip + ")* " 
-    else
-      code = code.rstrip + "* "
-    end    
-    code
+    code.rstrip + "* "
   end
 
-  def join_negativelookahead neg, out_of_array = true
+  def join_negativelookahead neg
+    @neg_idt = 1
     code = send("join_#{neg.child.class.name.downcase}", neg.child)
-    ary = neg.child
-    if judge_param code, ary
-      code = "!(" + code.rstrip + ") " 
-    else
-      code = "!" + code.rstrip + " "
-    end    
-    code
+    @neg_idt = 0
+    code[/\A\s*/] + "!" + code[/\S(.|\n)*\Z/].rstrip + " "
   end
 
-  def join_oneormore one, out_of_array = true
-    code = send("join_#{one.child.class.name.downcase}", one.child)
-    ary = one.child
-    if judge_param code, ary
-      code = "(" + code.rstrip + ")+ "
-    else
-      code = code.rstrip + "* "
-    end
-    code
+  def join_oneormore one
+    send("join_#{one.child.class.name.downcase}", one.child).rstrip + "* "
   end
 
-  def join_choice choices, out_of_array = true
-    @indents.push @indents.last + @idt
-    code = "\n" + " "*(@indents.last+1) + "(" + send("join_#{choices.child[0].class.name.downcase}", choices.child[0], false)
+  def join_choice choices
+    @indents.push @indents.last + @idt - @neg_idt
+    code = "\n" + " "*(@indents.last+1) + "(" + send("join_#{choices.child[0].class.name.downcase}", choices.child[0])
 #      @indent[-1] -= 1
-    choices.child.drop(1).each{|sym| code += "\n" + " "*@indents.last + "/ " + send("join_#{sym.class.name.downcase}", sym, false) }
+    choices.child.drop(1).each{|sym| code += "\n" + " "*@indents.last + "/ " + send("join_#{sym.class.name.downcase}", sym) }
     @indents.pop
     code.rstrip + ")"
   end
 
-  def join_nilclass sym, out_of_array = true
+  def join_nilclass sym
     ""
-  end
-
-  def judge_param code, ary
-    code != '(' && (ary.size > 1 && ary.find{|a| a.is_a?(String)} \
-      || (ary.size == 1 && ary.find{|a| a.is_a?(Array) && a.size > 1 && a.find{|b| b.is_a?(String)}}))
   end
 end
